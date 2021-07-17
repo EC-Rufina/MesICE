e649a1de003f815eb3a0271abc01b5354d52d9c9
EC-Rufina <85004601+EC-Rufina@users.noreply.github.com>
2021-07-17 18:02:44 +0800

    Update presence-absence-NJ.R

--- presence-absence-NJ.R
+++ presence-absence-NJ.R
@@ -1,6 +1,8 @@
 #these commands was used to produce the tree in figure S7
 #the purpose of this tree is simply to cluster ICEs based on their accessory genes, indipendently by the evolutionary processes by which these genes are present or absent in each ICE
 #the tree does not describe the phylogeny of the ICEs
+## these are commands for R ##
+
 
 library (ape)
 library(phangorn)
